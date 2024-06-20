fe6d9a3087af50f7dde5a254c030619b8818e503
VlasenkoHelena <33989161+VlasenkoElena@users.noreply.github.com>
2024-06-20 15:28:25 +0200

    init app

---
+++ InstagramApp/Core/TapBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  InstagramApp
+//
+//  Created by Helen on 20.06.2024.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
