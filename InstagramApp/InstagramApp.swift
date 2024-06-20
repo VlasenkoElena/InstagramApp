0781537523693ac43dcedcc8ac22557160d44039
VlasenkoHelena <33989161+VlasenkoElena@users.noreply.github.com>
2024-06-20 15:28:42 +0200

    init

---
+++ InstagramApp/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppApp.swift
+//  InstagramApp
+//
+//  Created by Helen on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
